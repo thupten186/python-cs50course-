3c24d2ddff37c7d080b44187533d1441a5230eed
thupten186 <thuptengrg186@gmail.com>
2022-08-07 23:35:43 -0400

    lambda.py

---
+++ lambda.py
@@ -0,0 +1,7 @@
+# people=[
+#     {"Name":"Harry","House":"kathmandu"},
+#         {"Name":"Ram","house":"Lalitpur"},
+#         {"Name":"Shyam","house":"Bhaktapur"}
+# ]
+# people.sort(key=lambda person:person["Name"])
+# print(people)
